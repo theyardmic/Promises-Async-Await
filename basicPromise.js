56d4f5ab871af1825143e2971f92d99e400245f5
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-10-14 17:37:58 +0300

    Update basicPromise.js

--- basicPromise.js
+++ basicPromise.js
@@ -1,5 +1,10 @@
 let blc = 5;
 let txn = new Promise(function(resolve, reject) {
-  
+ if (blc < 10) {
+   resolve("Your Balance is too low!");
+ }
+    else {
+      reject("Sufficient balance available!");
+    }
   
 });
